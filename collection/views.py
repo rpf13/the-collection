30bb0ab28d2c83b599513ca5d21bb19658a18177
rpf13 <rpf13@me.com>
2023-04-12 14:12:44 +0000

    feat: Add view for collection_list
    This commit will add the first view to display the collection_list.
    It will be the base for all other upcoming views.

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Collection
 
-# Create your views here.
 
-
+# Generic view to verify allauth - TO-BE-DELETED
 def home(request):
     return HttpResponse("Welcome to my Django app")
+
+
+# View to display collections list of a user
+class CollectionListView(LoginRequiredMixin, ListView):
+    model = Collection
+    template_name = 'collection_list.html'
+    context_object_name = 'collections'
+    paginate_by = 6
+
+    def get_queryset(self):
+        return Collection.objects.filter(
+            user=self.request.user
+            ).order_by('-created_on')
