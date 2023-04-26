2965ea8e8ab2b387f0462650a8d5e367aa5ff96a
rpf13 <rpf13@me.com>
2023-04-26 19:48:15 +0000

    fix: Correct path for static files directory

--- main/settings.py
+++ main/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 # Hosted on Whitenoise
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA Files on Cloudinary
