8307d7a4c78728964fc45e741c1e19a8280dcd70
rpf13 <rpf13@me.com>
2023-05-01 05:42:46 +0000

    feat: Add option to toggle local / remote db
    This commit will add a condition in the settings.py to toggle the local
    db, used for python testing. It's a simple condition, which becomes
    active, if the DATABASE_URL in the env.py gets commented out.

--- main/settings.py
+++ main/settings.py
@@ -110,9 +110,24 @@ WSGI_APPLICATION = 'main.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
+# Database w. option to use local db for testing, otherwise cloudinary
+if "DATABASE_URL" in os.environ:
+    print("database = PostgreSQL via Heroku")
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    print("database = db.sqlite3")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
